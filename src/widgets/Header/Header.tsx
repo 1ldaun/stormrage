56642fff7fd441a2cab0516854441324c040c148
1ldaun <sharfull33@gmail.com>
2023-04-03 02:37:01 +0300

    feat: added icons to mobile header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import cx from "classnames"
 import logoImg from '../../shared/img/logo-grey2.png'
 import styles from './Header.module.scss'
 
 const Header = () => {
+    const [isMobileMenuActive, setIsMobileMenuActive] = useState(false)
+
+    const mobileMenuHandler = () => setIsMobileMenuActive(prev => !prev);
+
     return (
         <>
             <div className={styles.wrapper}>
@@ -12,24 +16,28 @@ const Header = () => {
                     <li className={styles.navigation__item}>Works</li>
                     <li className={styles.navigation__item}>About me</li>
                 </ul>
-                <div className={styles.mobileNavigation}>
+                <div className={styles.mobileNavigation} onClick={mobileMenuHandler}>
                     <span className={cx(styles.mobileNavigation__item, styles.mobileNavigation__item_top)}/>
                     <span className={cx(styles.mobileNavigation__item, styles.mobileNavigation__item_middle)}/>
                     <span className={cx(styles.mobileNavigation__item, styles.mobileNavigation__item_bottom)}/>
                 </div>
             </div>
-            <div className={styles.mobileScreen}>
+            {isMobileMenuActive && <div className={styles.mobileScreen}>
                 <ul className={styles.mobileScreen__nav}>
                     <li className={styles.mobileScreen__nav__item}>Home</li>
                     <li className={styles.mobileScreen__nav__item}>Works</li>
                     <li className={styles.mobileScreen__nav__item}>About me</li>
                 </ul>
                 <div className={styles.mobileScreen__icons}>
-                    <a href="https://t.me/d1sinterested" target="_blank" rel="noreferrer" ><i className="fab fa-telegram-plane"></i></a>
-                    <a href="https://vk.com/dis1nterested" target="_blank" rel="noreferrer" ><i className="fa-brands fa-vk"></i></a>
-                    <a href="mailto:sharfull33@gmail.ru" target="_blank" rel="noreferrer" ><i className="far fa-envelope"></i></a>
+                    <a href="https://t.me/d1sinterested" target="_blank" rel="noreferrer" ><i className="fab fa-telegram-plane"/></a>
+                    <a href="https://vk.com/dis1nterested" target="_blank" rel="noreferrer" ><i className="fa-brands fa-vk"/></a>
+                    <a href="mailto:sharfull33@gmail.ru" target="_blank" rel="noreferrer" ><i className="far fa-envelope"/></a>
+                </div>
+                <div className={styles.mobileScreen__close} onClick={mobileMenuHandler}>
+                    <i className="fa-solid fa-xmark"/>
                 </div>
             </div>
+            }
         </>
     );
 };

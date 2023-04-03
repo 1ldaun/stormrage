2cc1e333c54b243ebde0cf7e9fa17a9699da8a83
1ldaun <sharfull33@gmail.com>
2023-04-04 01:23:18 +0300

    feat: added TopSection widget

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "../../widgets/Header/Header";
 import styles from "./MainPage.module.scss"
+import TopSection from "../../widgets/TopSection/TopSection";
 
 const MainPage = () => {
     return (
         <div className={styles.wrapper}>
             <Header/>
+            <TopSection/>
         </div>
     );
 };
---
+++ src/widgets/TopSection/TopSection.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./TopSection.module.scss"
+import bgLogo from "../../shared/img/logo512.png"
+
+const TopSection = () => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.background}>
+                <img src={bgLogo} alt="background"/>
+            </div>
+            <div className={styles.infoBlock}>
+                <p className={styles.infoBlock__name}>Ildan Sharifullin</p>
+                <p className={styles.infoBlock__description}>Front-end developer</p>
+                <input type="button" className={styles.infoBlock__button} value="About me"/>
+            </div>
+            <div className={styles.socialBlock}>
+                <a href="https://t.me/d1sinterested" target="_blank" rel="noreferrer" ><i className="fab fa-telegram-plane"/></a>
+                <a href="https://vk.com/dis1nterested" target="_blank" rel="noreferrer" ><i className="fa-brands fa-vk"/></a>
+                <a href="mailto:sharfull33@gmail.ru" target="_blank" rel="noreferrer" ><i className="far fa-envelope"/></a>
+            </div>
+        </div>
+    );
+};
+
+export default TopSection;

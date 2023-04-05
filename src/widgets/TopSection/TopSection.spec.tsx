6ac3fa0aa443a5668b669de851b07bb683fc98a7
1ldaun <sharfull33@gmail.com>
2023-04-05 18:42:53 +0300

    feat: added test for TopSection

---
+++ src/widgets/TopSection/TopSection.spec.tsx
@@ -0,0 +1,21 @@
+import {configure, mount} from "enzyme";
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+import TopSection from "./TopSection";
+
+configure({ adapter: new Adapter() });
+
+describe("<TopSection/>", () => {
+    it("Rendering", () => {
+        const wrapper = mount(<TopSection/>);
+        expect(wrapper.exists()).toBeTruthy();
+
+        const background = wrapper.find(".background");
+        expect(background.exists()).toBeTruthy();
+
+        const infoBlock = wrapper.find(".infoBlock");
+        expect(infoBlock.exists()).toBeTruthy();
+
+        const socialBlock = wrapper.find(".socialBlock");
+        expect(socialBlock.exists()).toBeTruthy();
+    });
+})

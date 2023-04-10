3a82871b72c526099bc8b56655a0c47219c471be
1ldaun <sharfull33@gmail.com>
2023-04-10 23:20:25 +0300

    feat: added Footer widget

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,17 +4,19 @@ import styles from "./MainPage.module.scss"
 import TopSection from "../../widgets/TopSection/TopSection";
 import WorksSection from "../../widgets/WorksSection/WorksSection";
 import InfoSection from "../../widgets/InfoSection/InfoSection";
+import Footer from "../../widgets/Footer/Footer";
 
 const MainPage = () => {
     const [isInfoSectionActive, setIsInfoSectionActive] = useState(false);
 
     return (
-        <div className={styles.wrapper}>
-            <Header setInfoSectionActive={setIsInfoSectionActive}/>
-            <TopSection/>
-            <WorksSection/>
-            <InfoSection isActive={isInfoSectionActive} setIsActive={setIsInfoSectionActive}/>
-        </div>
+            <div className={styles.wrapper}>
+                <Header setInfoSectionActive={setIsInfoSectionActive}/>
+                <TopSection setInfoSectionActive={setIsInfoSectionActive}/>
+                <WorksSection/>
+                <InfoSection isActive={isInfoSectionActive} setIsActive={setIsInfoSectionActive}/>
+                <Footer/>
+            </div>
     );
 };
 
---
+++ src/widgets/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./Footer.module.scss"
+import logoImg from "../../shared/img/logo-white.png"
+
+const Footer = () => {
+    return (
+        <div className={styles.wrapper}>
+            <img className={styles.logo} src={logoImg} alt="logo"/>
+            <ul className={styles.navigation}>
+                <li className={styles.navigation__item}>
+                    <a href="https://t.me/d1sinterested" target="_blank" rel="noreferrer" className={styles.navigation__link}>Telegram</a>
+                </li>
+                <li className={styles.navigation__item}>
+                    <a href="https://vk.com/dis1nterested" target="_blank" rel="noreferrer" className={styles.navigation__link}>Vkontakte</a>
+                </li>
+                <li className={styles.navigation__item}>
+                    <a href="https://github.com/1ldaun" target="_blank" rel="noreferrer" className={styles.navigation__link}>GitHub</a>
+                </li>
+            </ul>
+            <p className={styles.copyright}>© 2023 Ildan Sharifullin</p>
+        </div>
+    );
+};
+
+export default Footer;

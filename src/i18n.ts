5e05e23b62b27e686402b8cb23a537429d67e53b
1ldaun <sharfull33@gmail.com>
2023-04-16 16:04:28 +0300

    fix: changed default language

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,5 @@
 import {initReactI18next} from "react-i18next";
 import i18n from "i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
     en: {
@@ -59,9 +58,9 @@ const resources = {
 
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
-    .use(LanguageDetector)
     .init({
         resources,
+        lng: "en",
         fallbackLng: "en",
         interpolation: {
             escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape

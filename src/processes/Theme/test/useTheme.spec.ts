3f70f6d80cb53e829b651cfd2293623f80d96c90
stormrage-web <sharfull33@gmail.com>
2023-05-12 00:51:41 +0300

    fix: fixed UT for theme hook

--- src/processes/Theme/test/useTheme.spec.ts
+++ src/processes/Theme/test/useTheme.spec.ts
@@ -5,21 +5,21 @@ describe("useTheme hook", () => {
 	it("Functionality", () => {
 		const { result, rerender } = renderHook(() => useTheme());
 
-		expect(result.current.theme).toEqual(ThemeEnum.default);
-		expect(document.documentElement.getAttribute("data-theme")).toEqual(
-			ThemeEnum.default,
-		);
-		expect(localStorage.getItem("app-theme")).toEqual(ThemeEnum.default);
-
-		act(() => {
-			result.current.setTheme(ThemeEnum.dark);
-		});
-		rerender();
-
 		expect(result.current.theme).toEqual(ThemeEnum.dark);
 		expect(document.documentElement.getAttribute("data-theme")).toEqual(
 			ThemeEnum.dark,
 		);
 		expect(localStorage.getItem("app-theme")).toEqual(ThemeEnum.dark);
+
+		act(() => {
+			result.current.setTheme(ThemeEnum.default);
+		});
+		rerender();
+
+		expect(result.current.theme).toEqual(ThemeEnum.default);
+		expect(document.documentElement.getAttribute("data-theme")).toEqual(
+			ThemeEnum.default,
+		);
+		expect(localStorage.getItem("app-theme")).toEqual(ThemeEnum.default);
 	});
 });
